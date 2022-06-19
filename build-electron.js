6d0077f3e1c1e1e48e51179a8a8471f4713b6891
Kelp <kelp@phate.org>
2022-06-19 12:26:23 +0800

    chore: add productName and bundleID

--- build-electron.js
+++ build-electron.js
@@ -7,8 +7,6 @@ builder
 		win: ['portable'],
 		mac: ['dmg'],
 		config: {
-			appId: 'io.github.kelp404.electron-s3-file-manager',
-			productName: 'Electron S3 File Manager',
 			copyright: 'Copyright © 2022 kelp404',
 			directories: {
 				output: 'dist',
