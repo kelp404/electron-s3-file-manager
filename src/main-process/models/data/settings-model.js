7aad440831b92734f58779f3956e1f422f510df4
Kelp <kelp@phate.org>
2022-06-23 21:10:35 +0800

    feat: encrypt S3 SecretAccessKey

--- src/main-process/models/data/settings-model.js
+++ src/main-process/models/data/settings-model.js
@@ -1,8 +1,14 @@
+const crypto = require('crypto');
 const {DataTypes} = require('sequelize');
 const {connectDatabase} = require('../../common/database');
+const utils = require('../../common/utils');
 
 const {sequelize} = connectDatabase();
 const attributes = {
+	cryptoIv: {
+		type: DataTypes.STRING,
+		allowNull: true,
+	},
 	accessKeyId: {
 		type: DataTypes.STRING,
 		allowNull: true,
@@ -10,6 +16,21 @@ const attributes = {
 	secretAccessKey: {
 		type: DataTypes.STRING,
 		allowNull: true,
+		get() {
+			return utils.decrypt({
+				value: Buffer.from(this.getDataValue('secretAccessKey'), 'base64'),
+				iv: Buffer.from(this.getDataValue('cryptoIv'), 'base64'),
+			}).toString();
+		},
+		set(value) {
+			const iv = crypto.randomBytes(16);
+
+			this.setDataValue('cryptoIv', iv.toString('base64'));
+			this.setDataValue(
+				'secretAccessKey',
+				utils.encrypt({value: Buffer.from(value, 'utf8'), iv}).toString('base64'),
+			);
+		},
 	},
 	region: {
 		type: DataTypes.STRING,
