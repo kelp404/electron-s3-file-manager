d746b26e161aa9726c4b5f68b2290394a930b125
Kelp <kelp@phate.org>
2022-08-07 13:51:48 +0800

    feat: add root breadcrumb

--- src/renderer-process/components/objects.js
+++ src/renderer-process/components/objects.js
@@ -3,6 +3,8 @@ const React = require('react');
 const OBJECT_TYPE = require('../../shared/constants/object-type');
 const STORAGE_CLASS = require('../../shared/constants/storage-class');
 const utils = require('../common/utils');
+const store = require('../common/store');
+const {STORE_KEYS} = require('../common/constants');
 const Base = require('./shared/base');
 
 module.exports = class S3Settings extends Base {
@@ -21,12 +23,43 @@ module.exports = class S3Settings extends Base {
 		}),
 	};
 
+	constructor(props) {
+		super(props);
+
+		const settings = store.get(STORE_KEYS.SETTINGS);
+
+		this.state.breadcrumb = {
+			items: [
+				{
+					id: Math.random().toString(36),
+					title: settings.bucket,
+					urlParams: {dirname: null},
+				},
+			],
+		};
+	}
+
 	render() {
+		const {breadcrumb} = this.state;
+
 		return (
 			<div className="row">
 				<div className="col-12">
 					<div className="d-flex align-items-center justify-content-between mb-3">
 						{/* Breadcrumb */}
+						<nav>
+							<ol className="breadcrumb mb-0">
+								{
+									breadcrumb.items.map(item => (
+										<li key={item.id} className="breadcrumb-item">
+											<a href="#">
+												{item.title}
+											</a>
+										</li>
+									))
+								}
+							</ol>
+						</nav>
 					</div>
 				</div>
 			</div>
