0e17005713261eb47a0e70168c5496d810d0a152
Kelp <kelp@phate.org>
2022-10-24 18:45:13 +0800

    test: add tests for utils.decrypt()

--- __tests__/main-process/common/utils.js
+++ __tests__/main-process/common/utils.js
@@ -7,13 +7,33 @@ describe('encrypt', () => {
 			iv: Buffer.from('31682eec1f9655da345101e94ca741ba', 'hex'),
 		});
 
-		expect(result).toMatchSnapshot();
+		expect(result.toString('hex')).toMatchSnapshot();
 	});
 
 	test('encrypt string with invalid iv', () => {
 		expect(() => {
 			utils.encrypt({
 				value: Buffer.from('test value'),
+				iv: Buffer.from('31682eec1f9655da345101e94ca741', 'hex'),
+			});
+		}).toThrow('Invalid initialization vector');
+	});
+});
+
+describe('decrypt', () => {
+	test('decrypt string with iv', () => {
+		const result = utils.decrypt({
+			value: Buffer.from('8595b720aebb469f8ce1cb1c5982a813', 'hex'),
+			iv: Buffer.from('31682eec1f9655da345101e94ca741ba', 'hex'),
+		});
+
+		expect(result.toString()).toMatchSnapshot();
+	});
+
+	test('encrypt string with invalid iv', () => {
+		expect(() => {
+			utils.decrypt({
+				value: Buffer.from('8595b720aebb469f8ce1cb1c5982a813', 'hex'),
 				iv: Buffer.from('31682eec1f9655da345101e94ca741', 'hex'),
 			});
 		}).toThrow('Invalid initialization vector');
